07e7bf79679918e962c243a3654fa842150c0b54
boris-szl <boris.szelcsanyi@outlook.com>
2023-01-02 19:50:07 +0100

    feat: add logic for generating and verifying jwt tokens

--- go-projects/jwt-authentication-golang/go.mod
+++ go-projects/jwt-authentication-golang/go.mod
@@ -1,3 +1,8 @@
 module example/jwt-authentication-golang
 
 go 1.19
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.4.3
+	github.com/joho/godotenv v1.4.0
+)
--- go-projects/jwt-authentication-golang/main.go
+++ go-projects/jwt-authentication-golang/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
-
 	"github.com/golang-jwt/jwt/v4"
+	_ "github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
 )
 
 type Message struct {
@@ -21,21 +24,54 @@ func handlePage(writer http.ResponseWriter, request *http.Request) {
   if err != nil {
     return
   }
-  err := json.NewEncoder(writer).Encode(message)
+  err = json.NewEncoder(writer).Encode(message)
   if err != nil {
     return
   }
 }
 
+func Config(key string) string {
+  // load .env file
+  err := godotenv.Load(".env")
+  if err != nil {
+    fmt.Print("Error loading .env file")
+  }
+  return os.Getenv(key)
+}
+
+func generateJWT() (string, error) {
+  token := jwt.New(jwt.SigningMethodEdDSA)
+  claims := token.Claims.(jwt.MapClaims)
+  claims["exp"] = time.Now().Add(10 * time.Minute)
+  claims["authorized"] = true
+  claims["user"] = "username"
+  
+  var sampleSecretKey = []byte(Config("JWT_SECRET"))
+  
+  tokenString, err := token.SignedString(sampleSecretKey)
+  if err != nil {
+    return "", err
+  }
+
+  return tokenString, nil
+}
+
+func verifyJWT(endpointHandler func(writer http.ResponseWriter, request *http.Request)) http.HandlerFunc {
+
+  return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+
+  })
+
+}
+
 func main() {
+  /*
   http.HandleFunc("/home", handlePage)
   err := http.ListenAndServe(":8080", nil)
   if err != nil {
     log.Println("There was an error listening on port :8080", err)
   }
-
+  */
 }
-
-
 
 
